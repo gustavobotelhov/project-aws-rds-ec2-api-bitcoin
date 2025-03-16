ec7fa8f8628563ebd8d38582244708c387f4efc7
unknown <gu-botelho@hotmail.com>
2025-03-16 18:38:50 -0300

    updating fetch_1

--- src/fetch_1.py
+++ src/fetch_1.py
@@ -10,7 +10,7 @@ url = 'https://sandbox-api.coinmarketcap.com/v1/cryptocurrency/quotes/latest'
 
 parameters = {
  'symbol': 'BTC',  # Identificando o Bitcoin pelo símbolo
- 'convert': 'USD'  # Convertendo a cotação para USD
+ 'convert': 'BRL'  # Convertendo a cotação para USD
 }
 
 headers = {
@@ -21,9 +21,20 @@ headers = {
 session = Session()
 session.headers.update(headers)
 
-response = session.get(url, params=parameters)
+def get_current_quote():
+  try:
+    response = session.get(url, params=parameters)
+    data = json.loads(response.text)
 
-data = response.json()
+    if 'data' in data and "BTC" in data["data"]:
+      quote_brl = data["data"]["BTC"]["quote"]["BRL"]["price"]
+      last_update = data["data"]["BTC"]["quote"]["BRL"]["last_updated"]
+    else: 
+      print("Error retrieving bitcoin quote")
 
-print(data)
+    print(f"BTC - BRL current quote: {quote_brl:.2f}")
+    print(f"The last updated ocurred at {last_update}")
+  except (ConnectionError, Timeout, TooManyRedirects) as e: 
+    print(f"Connectcion error: {e}")
 
+get_current_quote()
